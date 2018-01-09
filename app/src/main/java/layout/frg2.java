ffd0523ec746ace79b21829270c5df487ec66ad4
wexos <nani4488reddy@gmail.com>
2018-01-09 19:07:10 +0530

    Initial commit

---
+++ app/src/main/java/layout/frg2.java
@@ -0,0 +1,31 @@
+package layout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ranjeet.fragments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class frg2 extends Fragment {
+
+
+    public frg2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_frg2, container, false);
+        return v;
+    }
+
+}
